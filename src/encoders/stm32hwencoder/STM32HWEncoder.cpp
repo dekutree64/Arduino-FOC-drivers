f3f4997c705ecd55be4d50dd7e8939496787fbdd
dekutree64 <54822734+dekutree64@users.noreply.github.com>
2026-07-16 01:14:46 -0500

    Update STM32HWEncoder.cpp

--- src/encoders/stm32hwencoder/STM32HWEncoder.cpp
+++ src/encoders/stm32hwencoder/STM32HWEncoder.cpp
@@ -3,6 +3,7 @@
 #if defined(_STM32_DEF_)
 
 #include "drivers/hardware_specific/stm32/stm32_mcu.h"
+#include "drivers/hardware_specific/stm32/stm32_timerutils.h"
 
 /*
   HardwareEncoder(int cpr)
@@ -28,7 +29,14 @@ float STM32HWEncoder::getSensorAngle() {
   Set the current angle using CNT register
 */
 void STM32HWEncoder::setCurrentAngle(float set_angle) {
-    encoder_handle.Instance->CNT = set_angle * cpr / _2PI;
+    angle_prev = _normalizeAngle(set_angle);
+    full_rotations = (int32_t)((set_angle - angle_prev) / _2PI);
+    float check_rounding_error = full_rotations * _2PI + angle_prev;
+    if (check_rounding_error > set_angle + _PI)
+        full_rotations -= 1;
+    else if (check_rounding_error < set_angle - _PI)
+        full_rotations += 1;
+    encoder_handle.Instance->CNT = (int32_t)(angle_prev * cpr / _2PI);
 }
 /*
   Modify encoder count directly
